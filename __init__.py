9514b8c45dafe7c8ed46d1da3a1313ff27cd211a
Andreas Reinle <gras@web.de>
2020-03-26 23:17:52 +0000

    ad download mycroft-transalte

--- __init__.py
+++ __init__.py
@@ -12,20 +12,29 @@ class PootleSync(MycroftSkill):
         self.lang_path = self.settings.get('lang_path') \
             if self.settings.get('lang_path') else self.file_system.path+"/"+self.lang+"/"
         self.log.info("init")
+        self.install_translate()
+
+    def install_translate(self):
+        if not os.path.isdir(self.file_system.path+"/mycroft-translate-master"):
+            wget.download('https://github.com/MycroftAI/mycroft-translate/archive/master.zip', self.file_system.path)
+            with zipfile.ZipFile(self.file_system.path+"/mycroft-translate-master.zip",'r') as zfile:
+                zfile.extractall(self.file_system.path+"/test/")
 
     @intent_file_handler('sync.pootle.intent')
     def handle_sync_pootle(self, message):
-        self.poodle_loader()
+        self.poodle_downloader()
 
-    def poodle_loader(self):
+    def poodle_downloader(self):
         self.log.info("start download")
         if os.path.isfile(self.file_system.path+"/"+self.lang[:-3]+".zip"):
             os.remove(self.file_system.path+"/"+self.lang[:-3]+".zip")
         wget.download("https://translate.mycroft.ai/export/?path=/"+self.lang[:-3], self.file_system.path+"/"+self.lang[:-3]+".zip")
         with zipfile.ZipFile(self.file_system.path+"/"+self.lang[:-3]+".zip",'r') as zfile:
                 zfile.extractall(self.file_system.path)
-        ##data = requests.get("https://translate.mycroft.ai/export/?path=/"+self.lang[:-3]+"/mycroft-skills/")
-        ##data.encoding = 'utf-8'
+        self.speak_dialog('sync.pootle')
+
+    def poodle_match(self):
+        pass
         ##sentence = "\n".join(re.findall(r'(msgstr ".*")', data.text)).replace("msgstr", "")
         #sentence = re.sub(r'["\d]|{\n.*}\n', '', sentence).replace('\n \n','\n').replace('\n \n','\n')
         #sentence = sentence.replace('\n \n','\n').replace('\n \n','\n').replace('\n \n','\n')
@@ -34,7 +43,6 @@ class PootleSync(MycroftSkill):
         #sentence = sentence.split("\n")
         #self.writing_sentence(sentence)
         #self.log.info(sentence)
-        self.speak_dialog('sync.pootle')
 
     def writing_sentence(self, sentence, filename="test.txt"):
         sentence = "\n".join(sentence)
